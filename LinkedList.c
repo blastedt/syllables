db1968dfc1c37b981c58ffd7e0ec47e2ffc0564a
tim <tim>
2016-10-22 21:51:16 -0400

    Better implementation of Linked List, allows for size queries

--- LinkedList.c
+++ LinkedList.c
@@ -2,72 +2,138 @@
 #include <stdio.h>
 
 #include "LinkedList.h"
-#include "Syllable.h"
-void* linked_list_get(LinkedList* list, int index) {
-	if (list == NULL || index < 0) return 0;		//index out of range
-	if (index == 0) return (*list).data;
-	return linked_list_get((*list).tail, index-1);
+
+/**
+* Create a new linked list and return a pointer to it.
+**/
+LinkedList* linked_list_create() {
+	LinkedList* list = (LinkedList*) malloc(sizeof(LinkedList));
+	list->head = NULL;
+	list->tail = NULL;
+	list->size = 0;
 }
 
+//Make a new linked list node
+LinkedListNode* linked_list_node_create() {
+	LinkedListNode* node = (LinkedListNode*) malloc(sizeof(LinkedListNode));
+	node->head = NULL;
+	node->tail = NULL;
+	node->data = NULL;
+}
+
+/**
+* Add an element to a linked list.
+**/
 void linked_list_add(LinkedList* list, void* data) {
-	if ((*list).tail == NULL) {
-		LinkedList* new_element = (LinkedList*) malloc(sizeof(LinkedList));
-		(*list).tail = new_element;
-		(*new_element).head = list;
-	} else {
-		linked_list_add((*list).tail, data);
+	LinkedListNode* new_node = linked_list_node_create();
+	new_node->data = data;
+	LinkedListNode* old_tail = list->tail;
+	list->tail = new_node;
+	if (list->size > 0) {
+		old_tail->tail = new_node;
+		new_node->head = old_tail;
+	}  else {
+		//size = 0
+		list->head = new_node;
 	}
+	list->size = list->size + 1;
 }
 
-LinkedList* linked_list_new(void* data) {
-	LinkedList* list = malloc(sizeof(LinkedList));
-	(*list).head = NULL;
-	(*list).tail = NULL;
-	list->data = data;
-	return list;
+//Get the element at index
+void* linked_list_get(LinkedList* list, int index) {
+	if (index >= list->size || index < 0) {
+		return NULL; //element not found
+	}
+	LinkedListNode* cur_node;
+	if (index < list->size/2) {
+		//first half of the list, so iterate forwards
+		cur_node = list->head;
+		while (index > 0) {
+			cur_node = cur_node->tail;
+			index--;
+		}
+	} else {
+		//more than halfway along, so iterate backwards
+		cur_node = list->tail;
+		index = list->size - 1 - index;	//transform the index into distance from tail
+		while (index > 0) {
+			cur_node = cur_node->head;
+			index--;
+		}
+	}
+	return cur_node->data;
+
 }
 
+//append two linked lists
 void linked_list_append(LinkedList* a, LinkedList* b) {
-	if ((*a).tail == NULL) {
-		(*a).tail = b;
-		(*b).head = a;
-	} else {
-		linked_list_append((*a).tail, b);
-	}
+	atail = a->tail;
+	bhead = b->head;
+	btail = b->tail;
+
+	atail->tail = bhead;
+	bhead->head = atail; //set tail of A's tail to B's head, and vice versa
+
+	a->tail = btail;	//set tail of list A to B's tail
+
+	a->size = a->size + b->size;	//fix the size
+
+	free(b);			//free up the LinkedList b space
 }
 
-void linked_list_destroy(LinkedList* list) {
-	if ((*list).tail != NULL) {
-		linked_list_destroy((LinkedList*)(*list).tail);
+//Remove an element from a list and return its data
+void* linked_list_remove(LinkedList* list, int index) {
+	if (index >= list->size) {
+		return NULL; //element not found
+	}
+	LinkedListNode* cur_node;
+	if (index < list->size/2) {
+		//first half of the list, so iterate forwards
+		cur_node = list->head;
+		while (index > 0) {
+			cur_node = cur_node->tail;
+			index--;
+		}
+	} else {
+		//more than halfway along, so iterate backwards
+		cur_node = list->tail;
+		index = list->size - 1 - index;	//transform the index into distance from tail
+		while (index > 0) {
+			cur_node = cur_node->head;
+			index--;
+		}
+	}
+	LinkedListNode* head = cur_node->head;
+	LinkedListNode* tail = cur_node->tail;
+	head->tail = tail;
+	tail->head = head; //remove the node from the list by pointing the other nodes at each other
+	void* data = cur_node->data;
+	free(cur_node);		//free the node to prevent memory leaks
+	list->size = list->size - 1;
+	return data;
+}
+
+void linked_list_free(LinkedList* list) {
+	if (list->size > 0) {	//if there are elements in the list
+		LinkedListNode* cur_node = list->head;
+		while (cur_node) {	//cur_node will be nul/0 if we've run out of elements
+			LinkedListNode* next_node = cur_node->tail;
+			free(cur_node);
+			cur_node = next_node;
+		}
 	}
 	free(list);
 }
 
-void* linked_list_remove(LinkedList* list, int index) {		//will orphan reference if you free the first element. so. don't do that. yeah.
-	if (list == NULL || index < 0) return 0;		//index out of range
-	if (index == 0){
-		LinkedList* head = (*list).head;
-		LinkedList* tail = (*list).tail;
-		free(list);
-		if (head == NULL) {
-			printf("Fatal error: orphaned linked list elements (did you remove element zero?)");
-			exit(1);
+void linked_list_free_and_data(LinkedList* list) {
+	if (list->size > 0) {	//if there are elements in the list
+		LinkedListNode* cur_node = list->head;
+		while (cur_node) {	//cur_node will be nul/0 if we've run out of elements
+			LinkedListNode* next_node = cur_node->tail;
+			free(cur_node->data);
+			free(cur_node);
+			cur_node = next_node;
 		}
-		if (tail != NULL) {
-			//didn't remove last element
-			(*head).tail = tail;
-			(*tail).head = head;
-		} else {
-			(*head).tail = NULL;
-		}
-
-		return (*list).data;
-	} 
-	return linked_list_remove((*list).tail, index-1);
-}
-
-Syllable* search_for_syllable(LinkedList* list, char* name) {
-	if (list == NULL) return NULL;
-	if (!strcmp(name, ((Syllable*) (list->data))->name)) return (Syllable*) list->data;
-	return search_for_syllable(list->tail, name);
+	}
+	free(list);
 }
