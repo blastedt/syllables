d16e0cb91003e9747a0e83773c29ac53a5431fc4
Tim Rollet <tmr9996@rit.edu>
2016-11-06 17:44:15 -0500

    Move tests to src subfolder

---
+++ src/tests/LinkedList_tests.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "LinkedList.h"
+
+#define TEST_SIZE 1000
+void lots_of_adds() {
+	int* elements = malloc(sizeof(int) * TEST_SIZE);
+	srand((unsigned int) time(NULL));
+	LinkedList* list = linked_list_create();
+	for (int i = 0; i < TEST_SIZE; i++) {
+		elements[i] = rand();
+		linked_list_add(list, (void*) elements[i]);
+	}
+
+	for (int j = 0; j < TEST_SIZE; j++) {
+		int correct = elements[j];
+		int test = (int) linked_list_get(list, j);
+		if (correct != test) {
+			printf("Lots of adds failed: retrieved element not correct");
+			exit(1);
+		}
+	}
+
+	linked_list_free(list);
+}
+
+void get_reverse() { //does a lot of reverse gets
+	int* elements = malloc(sizeof(int) * TEST_SIZE);
+	srand((unsigned int) time(NULL));
+	LinkedList* list = linked_list_create();
+	for (int i = 0; i < TEST_SIZE; i++) {
+		elements[i] = rand();
+		linked_list_add(list, (void*) elements[i]);
+	}
+
+	for (int j = TEST_SIZE-1; j >= 0; j--) {
+		int correct = elements[j];
+		int test = (int) linked_list_get(list, j);
+		if (correct != test) {
+			printf("Lots of adds failed: retrieved element not correct");
+			exit(1);
+		}
+	}
+
+	linked_list_free(list);
+}
+
+
+
+int main() {
+	lots_of_adds();
+	printf("Lots of adds succeeded\n");
+	get_reverse();
+	printf("Get reverse succeeded");
+}
---
+++ src/tests/Syllable_tests.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "LinkedList.h"
+#include "Syllable.h"
+
+void syllable_search() {
+	
+}
